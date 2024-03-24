42276175d01a73ff7adfaf6f3eee62f62fad16aa
Anish Balami <anishbalami38@gmail.com>
2024-03-24 21:58:51 +0545

    final auth

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -5,17 +5,19 @@ import SidebarItem from './SidebarItem'
 import {MdGroups,MdOutlineExpandMore, MdOutlineOndemandVideo} from 'react-icons/md'
 import {AiOutlineShop} from "react-icons/ai"
 import {BsStopwatch} from 'react-icons/bs'
+import { useSession } from 'next-auth/react'
 
 const Sidebar = () => {
+     const{data: session}=useSession();
   return (
     <div className='hidden lg:inline-flex flex-col py-2 pl-2 max-w-xl lg:min-w-[320px]'>
         <div className='flex items-center space-x-2 py-3 pl-4 hover:bg-gray-200 rounded-l-xl cursor-pointer'>
         <Image 
-            src="https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/512px-Facebook_f_logo_%282021%29.svg.png?20210818083032"
+            src={session?.user.image}
             height={40}
             width={40}
             className='rounded-full cursor-pointer' />
-            <p className='hidden sm:inline-flex font-medium'>Anish Balami</p>
+            <p className='hidden sm:inline-flex font-medium'>{session?.user.name}</p>
         </div>
         <SidebarItem Icon={ImUsers} value="Friends"/>
         <SidebarItem Icon={MdGroups} value="Groups"/>
--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images:{
-    domains:["upload.wikimedia.org"],
+    domains:[
+      "upload.wikimedia.org",
+      "platform-lookaside.fbsbx.com"
+    ],
   }
 
 };
