bafa16282d6637a55d3588e1387e894fa4b33feb
Anish Balami <anishbalami38@gmail.com>
2024-03-24 14:51:37 +0545

    reupdate auth

--- components/Login.js
+++ components/Login.js
@@ -4,11 +4,17 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div flex flex-col items-center mx-auto>
+    <div className='flex flex-col items-center mx-auto py-20'>
+
+    
         <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Facebook_Logo_%282019%29.svg/800px-Facebook_Logo_%282019%29.svg.png"
-            height={240}
-            width={240} />
-        <a onClick={signIn} className='px-20 py-4 z-10 text-2xl cursor-pointer -mt-16 bg-blue-500 rounded-md text-white'>Login</a>
+            height={340}
+            width={340} />
+            <div className='flex items center py-20'>
+            <a onClick={signIn} className='flex px-20 py-4 z-10 text-2xl cursor-pointer -mt-16 bg-blue-500 rounded-md text-white'>Login</a>
+            </div>
+            
+        
     </div>
   )
 }
