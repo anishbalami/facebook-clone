9b71f271ca2279c4e6e98a5bd96e4b0be633efdc
Anish Balami <anishbalami38@gmail.com>
2024-03-24 14:40:13 +0545

    auth

---
+++ components/Login.js
@@ -0,0 +1,16 @@
+import { signIn } from 'next-auth/react'
+import Image from 'next/image'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div flex flex-col items-center mx-auto>
+        <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Facebook_Logo_%282019%29.svg/800px-Facebook_Logo_%282019%29.svg.png"
+            height={240}
+            width={240} />
+        <a onClick={signIn} className='px-20 py-4 z-10 text-2xl cursor-pointer -mt-16 bg-blue-500 rounded-md text-white'>Login</a>
+    </div>
+  )
+}
+
+export default Login
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Header from "@/components/Header";
+import Login from "@/components/Login";
 import Sidebar from "@/components/Sidebar";
+import { getSession } from "next-auth/react";
 import Head from "next/head";
 
 
-export default function Home() {
+export default function Home({ session }) {
+  if(!session) return <Login />;
   return (
    <div>
     <Head>
@@ -18,4 +21,12 @@ export default function Home() {
     </main>
    </div> 
   );
+}
+
+export async function getServerSideProps(context)
+{
+  const session = await getSession(context);
+  return {
+    props: { session },
+  };
 }
